0c142e1af805d1042c84b0d11cb20844c27e2c94
Niche <brendonhallows@googlemail.com>
2023-08-23 02:40:52 +0100

    Update LSS.py
    Run timing, better visability.

--- LSS.py
+++ LSS.py
@@ -4,8 +4,11 @@ import requests
 import random
 import string
 import time
+from colorama import Fore, init
 
 win = int()
+init(autoreset=True)
+
 def link():
     global adress
     adress = str()
@@ -37,18 +40,24 @@ def puller():
 
     try:
         urllib.request.urlretrieve(photo, tradress)
-        print("Success!")
+        print(Fore.GREEN+"Success!")
         win = win+1
         
     except:
-        print("image removed :(")
+        print(Fore.RED+"image removed :(")
 
 amount = int(input("How many links would you like to test: "))
+
+start = time.time()
 
 for i in range(amount):
     print((i+1),"/",amount)
     link()
     puller()
 
+end = time.time()
+
 print(win, "/",amount," successful")
-time.sleep(4)
+print("Average time per attempt: ",((end-start)/amount))
+print("Average time per successful image output: ",((end-start)/win))
+confirm = input("Press enter to close the program.")
